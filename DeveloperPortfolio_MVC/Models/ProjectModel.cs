6b980b019a5e7144f7a22a076fd91bdb0d39122f
Bishopper <muhammad.behnam@yahoo.com>
2023-04-07 05:23:57 +0430

    add projects View Components

---
+++ DeveloperPortfolio_MVC/Models/ProjectModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DeveloperPortfolio_MVC.Models
+{
+	public class ProjectModel
+	{
+		public long Id { get; set; }
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+
+		public ProjectModel(long id, string name, string description, string client, string image)
+		{
+			Id = id;
+			Name = name;
+			Description = description;
+			Client = client;
+			Image = image;
+		}
+	}
+}
---
+++ DeveloperPortfolio_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DeveloperPortfolio_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperPortfolio_MVC.ViewComponents
+{
+	public class ProjectsViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<ProjectModel>
+			{
+				new ProjectModel(1,"تاکسی","درخواست آنلاین تاکسی","محمد بهنام","project-1.jpg"),
+				new ProjectModel(2,"فود","درخواست آنلاین غذا","محمد بهنام","project-2.jpg"),
+				new ProjectModel(3,"مارکت","درخواست آنلاین تغذیه","محمد بهنام","project-3.jpg"),
+				new ProjectModel(4,"وانتسان","درخواست آنلاین وانت","محمد بهنام","project-4.jpg")
+			};
+			return View("_Projects",projects);
+		}
+	}
+}
