bee6c9acb03b8ad25f994b4b7c83f26633620e03
Bishopper <muhammad.behnam@yahoo.com>
2023-04-07 13:22:34 +0430

    Add Article to Project

---
+++ DeveloperPortfolio_MVC/Models/ArticlesModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DeveloperPortfolio_MVC.Models
+{
+	public class ArticlesModel
+	{
+		public long Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Image { get; set; }
+
+		public ArticlesModel(long id, string title, string description, string image)
+		{
+			Id = id;
+			Title = title;
+			Description = description;
+			Image = image;
+		}
+	}
+
+}
---
+++ DeveloperPortfolio_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DeveloperPortfolio_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperPortfolio_MVC.ViewComponents
+{
+	public class LatestArticlesViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var articles = new List<ArticlesModel>
+			{
+				new ArticlesModel(1,"آموزش کور","asp.net core mvc","blog-post-thumb-card-1.jpg"),
+				new ArticlesModel(2,"آموزش سی شارپ","csharp beginner to zero","blog-post-thumb-card-2.jpg"),
+				new ArticlesModel(3,"آموزش جنگو","Django site","blog-post-thumb-card-3.jpg"),
+				new ArticlesModel(4,"آموزش پایتون","Python For evryone","blog-post-thumb-card-4.jpg")
+			};
+			return View("_LatestArticles",articles);
+		}
+	}
+}
